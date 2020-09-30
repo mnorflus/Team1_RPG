0a083c7026860305129242482633cdaac48c69d8
Drqrd <justinm0623@gmail.com>
2020-09-30 12:21:37 -0400

    Commit of PlayerPlaceholder and PlayerMovement.cs

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(5f,20f)]
+    public float speed = 10f;
+
+    void Start()
+    {
+      
+    }
+    void Update()
+    {
+        transform.Translate(GetInput().normalized*speed*Time.deltaTime);
+
+    }
+
+    Vector3 GetInput()
+    { 
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
+            p_Velocity += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
+            p_Velocity += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            p_Velocity += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            p_Velocity += Vector3.right;
+        }
+
+        return p_Velocity;
+    }
+}
