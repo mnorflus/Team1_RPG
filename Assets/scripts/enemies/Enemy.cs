b29951080cccd9a8a285a1cc395e04a903423dcc
mnorflus <mrn16c@my.fsu.edu>
2020-10-26 18:34:17 -0400

    Enemy follow AI

--- Assets/scripts/enemies/Enemy.cs
+++ Assets/scripts/enemies/Enemy.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
